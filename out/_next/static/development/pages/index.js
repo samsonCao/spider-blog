31b23c2472601e2f0140b7de3ffecde7d447c22a
曹有志 <caoyouzhi@nanjids.com>
2019-10-03 19:44:30 +0800

    Deploy gh-pages

--- out/_next/static/development/pages/index.js
+++ out/_next/static/development/pages/index.js
@@ -24,19 +24,19 @@ var linkList = [{
   href: '/index',
   title: '首页'
 }, {
-  href: 'xxx',
+  href: '/index',
   title: 'seo文案'
 }, {
-  href: 'xxx',
+  href: '/index',
   title: '信息流文案'
 }, {
-  href: 'xxx',
+  href: '/index',
   title: 'sem文案'
 }, {
-  href: 'xxx',
+  href: '/index',
   title: '落地页文案'
 }, {
-  href: 'xxx',
+  href: '/index',
   title: '子弹头'
 }];
 
